3485053738b07a6aae568237a9d828bf03ba1419
sellorm <github@5vcc.com>
2020-12-26 14:05:18 +0000

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,49 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000200001010101010101010101010101010101010203030303080a040303030303080101040505070b090a010101010107090101010101063a090a020303030307090101010101063b090a060705050507090101020303070b090b06090c0c0c060901010607050505050307070303030709010106091b1c1c1d060505050505070901010607050505050d0a010a0a0a0609010106090a010a0a0a0a0a0a020a0609010106090a06070505050505050a0609010106090a06090a0c0c0c0c0c0c0609010106090106090a0607050505050709010106070307090a06090e0f101006090101040505050d0a040d10100f0e040d0101010101010101010101010101010101292a2c2d2c2b0a3c3c0a2a2c2d2a2c2b2011171717180a100f0a1117171718242015161a1536191919193816391516252715163915351313131337161a1516332015161a15161a1f1a1a1516391516242015161a1536171717173816391516242015163914131313131313121a1516242015161a1a1a1a1e1f1a1a391a1516252715361718321117171717171738163320151919163115191919191919191624201535131232141919131313133716242615162f22223428283422222e1516242715162411171719191717182615163320151624151919191919191626151624201412243d131313131313122714162421222223303030303030303021222223`, img`
+2222222222222222
+2......2.......2
+2....2.222222..2
+2222.2.2.......2
+2222.2.2.......2
+2....2.2..222..2
+2..............2
+2..2222........2
+2.......22222..2
+2..22222222.2..2
+2..2........2..2
+2..2..2222222..2
+2..2..2........2
+2.....2..2222..2
+2.....2..2222..2
+2222222..2222222
+2222222..2222222
+2.....2..2.....2
+2..2........2..2
+2..2........2..2
+2..2..2222..2..2
+2..2........2..2
+2..2........2..2
+2..2222.22222..2
+2....2.........2
+2....2.........2
+2....2.........2
+2..2222..2222..2
+2..2........2..2
+2..2........2..2
+2..2........2..2
+2222222222222222
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath4,sprites.castle.tilePath5,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tileGrass1,sprites.builtin.forestTiles0,sprites.castle.tileGrass3,sprites.castle.tilePath9,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundCenter,sprites.dungeon.floorLight0,sprites.builtin.forestTiles1,sprites.builtin.forestTiles2,sprites.builtin.forestTiles3,sprites.dungeon.floorLight3,sprites.dungeon.floorLightMoss,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest2,sprites.dungeon.stairNorth,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.floorDark2,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundSouthWest1,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.darkGroundSouthEast1,sprites.dungeon.floorLight1,sprites.castle.rock0,sprites.castle.rock1,sprites.castle.rock2,myTiles.tile1], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
